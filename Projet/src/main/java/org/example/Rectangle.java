33415ba1a17a41569da71eda0b30d8ca70478e6f
dorian dallon <dorian084270@gmail.com>
2024-09-16 11:00:36 +0200

    rectangle en fonction des points

--- Projet/src/main/java/org/example/Rectangle.java
+++ Projet/src/main/java/org/example/Rectangle.java
@@ -3,22 +3,29 @@ package org.example;
 public class Rectangle {
 
     // Variables pour les côtés
-    private double longueur;
-    private double largeur;
+    private Point p1;
+    private Point p2;
 
     // Constructeur
-    public Rectangle(double longueur, double largeur) {
-        this.longueur = longueur;
-        this.largeur = largeur;
+    public Rectangle(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public double longueur(){
+        return Math.abs(p2.getY() - p1.getY());
+    }
+    public double largeur(){
+        return Math.abs(p2.getX() - p1.getX());
     }
 
     // Méthode pour calculer l'aire
     public double getAire() {
-        return longueur * largeur;
+        return longueur() * largeur();
     }
 
     // Méthode pour calculer le périmètre
     public double getPerimetre() {
-        return 2 * (longueur + largeur);
+        return 2 * (longueur() + largeur());
     }
 }
