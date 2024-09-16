fcce57a87f2bc83128453f151c5fc5bef806a96c
Akram <oumimouneakram8480@gmail.com>
2024-09-16 10:14:15 +0200

    Création de la classe Point pour les distances.

---
+++ Projet/src/main/java/org/example/Point.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Point {
+
+        private double x;
+        private double y;
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public double getY() {
+            return y;
+        }
+        
+        public double distance(Point a) {
+            return Math.sqrt(Math.pow(this.x - a.getX(), 2) + Math.pow(this.y - a.getY(), 2));
+        }
+
+}
